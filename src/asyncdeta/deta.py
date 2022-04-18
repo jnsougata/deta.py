e9c321948dbd2ca2597b890dfe1a35b28abb73c4
Zen <jnsougata@gmail.com>
2022-04-18 08:23:42 +0530

    connection method to Deta

--- src/asyncdeta/deta.py
+++ src/asyncdeta/deta.py
@@ -1,12 +1,37 @@
+import asyncio
 import aiohttp
 from .base import _Base
 
 
 class Deta:
 
-    def __init__(self, *, project_key: str, session: aiohttp.ClientSession):
-        self._session = session
+    def __init__(self, *, project_key: str):
+        self._session = None
         self._project_key = project_key
 
+    async def connect(self, *, session: aiohttp.ClientSession = None, loop: asyncio.AbstractEventLoop = None):
+        """
+        creates a new session for Deta to make requests to the API
+        """
+        if session is None:
+            if loop:
+                self._session = aiohttp.ClientSession(loop=loop)
+            else:
+                self._session = aiohttp.ClientSession()
+        else:
+            self._session = session
+
+    async def close(self):
+        """
+        closes the existing session
+        """
+        await self._session.close()
+
     def base(self, name: str) -> _Base:
+        """
+        creates a new instance of the Base
+        """
         return _Base(name=name, deta=self)
+
+    def drive(self):
+        raise NotImplementedError("Drive is not implemented yet")
--- testing/main.py
+++ testing/main.py
@@ -4,13 +4,19 @@ import asyncio
 from src.asyncdeta import Deta, Field, Update
 
 
+
 async def main():
     token = os.getenv('DETA_TOKEN')
     session = aiohttp.ClientSession()
-    deta = Deta(project_key=token, session=session)
-    base = deta.base(name='123TEST')
-    await base.add_field(key='12345678', field=Field('YOUTUBE', {}), force=True)
-    await session.close()
+    deta = Deta(project_key=token)
+    await deta.connect()
+    try:
+        base = deta.base(name='123TEST')
+        await base.add_field(key='12345678', field=Field('YOUTUBE', ['ABC']), force=True)
+    except Exception as e:
+        print(e)
+    finally:
+        await deta.close()
 
 loop = asyncio.new_event_loop()
 loop.run_until_complete(main())
