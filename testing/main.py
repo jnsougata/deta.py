ba8ce91b10aa217b439e81ba7aef81f206211712
jnsougata <jnsougata@gmail.com>
2022-04-24 20:24:57 +0530

    better docs

--- testing/main.py
+++ testing/main.py
@@ -4,14 +4,12 @@ from src.asyncdeta import Deta, Field
 
 
 async def main():
-    deta = Deta(os.getenv("PROJECT_KEY"))
+    deta = Deta(os.getenv("DETA_TOKEN"))
     await deta.connect()
-    drive = deta.drive(name='test_123')
-    base = deta.base(name='test_123')
+    drive = deta.drive(name='123_drive')
+    base = deta.base(name='123_base')
     await base.put(key='test', field=Field(name='abc', value={'a': 1, 'b': 2}))
-    resp = await drive.download(file_name='song.mp3')
-    with open('song.mp3', 'wb') as f:
-        f.write(resp)
+    await drive.upload(file_name='song.mp3', local_path='/home/jnsougata/Downloads/song.mp3')
     await deta.close()
 
 
